cd68a6359fc661c258bdca93e1862e4a491349ca
Sunit Parekh <sunitp@thoughtworks.com>
2018-06-11 15:28:48 +0530

    hostname added

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+var os = require("os");
+var hostname = os.hostname();
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world !!!\n' + hostname + '\n');
 });
 
 app.listen(PORT, HOST);
